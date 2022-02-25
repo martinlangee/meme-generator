53a13c5916b66360e16e6f582dc531c05319ad7d
martin <martin.lange@csa-germany.de>
2022-02-25 14:14:33 +0100

    entered text is displayed on the image

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 function App() {
   const [currentMemeIdx, setCurrentMemeIdx] = useState(0);
   const [memeData, setMemeData] = useState([]);
+  const [textList, setTextList] = useState(["Sample text 1", "Sample text 2"]);
 
   const getMemeData = () => {
     Axios.get(`https://api.imgflip.com/get_memes`).then((resp) => {
@@ -26,9 +27,16 @@ function App() {
     setCurrentMemeIdx((prev) => prev + 1);
   };
 
+  const handleTextChange = (value, idx) => {
+    setTextList((prev) => [
+      ...prev.slice(0, idx),
+      value,
+      ...prev.slice(idx + 1),
+    ]);
+  };
+
   const currentImgPath = () => {
     const imgData = memeData[currentMemeIdx];
-    console.log(imgData);
     if (imgData) return imgData.url;
     else return "";
   };
@@ -37,18 +45,31 @@ function App() {
     <div className="App">
       <header className="App-header">Meme Generator</header>
       <div className="App">
-        <form action="">
-          <label for="text1">Text 1</label>
-          <input type="text" id="text1" name="text1" />
-          <label for="text2">Text 2</label>
-          <input type="text" id="text2" name="text2" />
-          <input type="submit" value="Submit" />
+        <form>
+          <label className="input-label" for="text1">
+            Text 1
+          </label>
+          <input
+            type="text"
+            onChange={(e) => handleTextChange(e.target.value, 0)}
+          />
+          <label className="input-label" for="text2">
+            Text 2
+          </label>
+          <input
+            type="text"
+            onChange={(e) => handleTextChange(e.target.value, 1)}
+          />
         </form>
         <div className="navigation">
           <button onClick={onPrev}>Previous</button>
           <button onClick={onNext}>Next</button>
         </div>
-        <img src={currentImgPath()} alt="Meme" />
+        <div className="imgContainer">
+          <label className="meme-text text1">{textList[0]}</label>
+          <label className="meme-text text2">{textList[1]}</label>
+          <img src={currentImgPath()} alt="Meme" />
+        </div>
       </div>
     </div>
   );
